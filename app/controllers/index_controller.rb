b3a5ea1b48c8f131d21456a5617b4f93a5f3a5d7
John Smyth <smythj6@gmail.com>
2014-07-08 17:33:47 +0100

    Some more UI changes

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,15 +1,17 @@
 class IndexController < ApplicationController
   def index
     tt = Git.open(".", :log => Logger.new(STDOUT))
-    @commit_array = []
+    @projects = {}
 
     tt.log.each do |commit|
       begin
-        commit_json = JSON.parse(commit.message)
-        @commit_array << {
-          commit_json: commit_json,
-          commit_obj: commit
-        }
+        commit_message_json = JSON.parse(commit.message)
+        project = commit_message_json["project"]
+        @projects[project] = [] unless @projects[project]
+
+        @projects[project] << commit_message_json.merge({
+          "author" => commit.author.name
+        })
       rescue JSON::ParserError
         puts "skipping non JSON commit message"
       end
